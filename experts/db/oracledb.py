d53d3471b47381585a77f84a05e1824e660a4517
John A Barneson <barne102@umn.edu>
2023-06-21 11:45:13 -0500

    Cleaned up oracledb.py variable names

--- experts/db/oracledb.py
+++ experts/db/oracledb.py
@@ -6,7 +6,7 @@ import os
 def connection():
     # Note that this approach to making a connection should not
     # require a tnsnames.ora config file.
-    u=os.environ.get('EXPERTS_DB_USER')
-    p=os.environ.get('EXPERTS_DB_PASS')
+    un=os.environ.get('EXPERTS_DB_USER')
+    pw=os.environ.get('EXPERTS_DB_PASS')
     cs=f'{os.environ.get("EXPERTS_DB_HOSTNAME")}/{os.environ.get("EXPERTS_DB_SERVICE_NAME")}'
-    yield oracledb.connect(user=u,password=p,dsn=cs)
+    yield oracledb.connect(user=un,password=pw,dsn=cs)
