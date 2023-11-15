6d163462baec6f5da53bc9cc2889c6fd3712584b
David Naughton <naughton@pop-os.localdomain>
2023-11-14 18:20:23 -0600

    Fixes and improves experts.api.context
    * Renames
      * size -> items_per_page
      * count -> total_items
    * Adds to OffsetResponseParser
      * items_per_page
      * offset
    * In the Context protocol, replaces settable attribtutes with properties,
      to keep mypy happy

--- experts/api/context.py
+++ experts/api/context.py
@@ -14,7 +14,7 @@ OffsetRequestParams = PMap
 
 class OffsetRequestParamsParser(Protocol):
     @staticmethod
-    def size(params:OffsetRequestParams) -> int:
+    def items_per_page(params:OffsetRequestParams) -> int:
         ...
 
     @staticmethod
@@ -30,7 +30,15 @@ OffsetResponse_contra = TypeVar('OffsetResponsee_contra', bound=OffsetResponse, 
 
 class OffsetResponseParser(Protocol, Generic[OffsetResponse_contra]):
     @staticmethod
-    def count(response:OffsetResponse_contra) -> int:
+    def total_items(response:OffsetResponse_contra) -> int:
+        ...
+
+    @staticmethod
+    def items_per_page(response:OffsetResponse_contra) -> int:
+        ...
+
+    @staticmethod
+    def offset(response:OffsetResponse_contra) -> int:
         ...
 
     @staticmethod
@@ -59,8 +67,6 @@ def default_retryable():
        return partial(
             retryable,
             retryable_status_codes=v(429, 500, 502, 503, 504),
-            # Old code that used requests:
-            #retryable_errors=(ConnectionError, ConnectTimeout, ReadTimeout),
             retryable_errors=(httpx.NetworkError, httpx.TimeoutException),
         )
 
@@ -71,51 +77,62 @@ class Context(Protocol):
     '''Common client configuration settings. Used by most functions.
     '''
 
-    httpx_client: httpx.Client
+    @property
+    def httpx_client(self) -> httpx.Client: ...
     '''An httpx.Client object.'''
 
-    timeout: Tuple[int, int]
+    @property
+    def timeout(self) -> Tuple[int, int]: ...
     '''A (connect timeout, read timeout) tuple. Required.'''
 
-    max_attempts: int
+    @property
+    def max_attempts(self) -> int: ...
     '''An integet maximum number of times to retry a request. Required.'''
 
     # TODO: Make a protocol for this function
-    #retryable: Callable = Factory(default_retryable)
-    retryable: Callable
+    @property
+    def retryable(self) -> Callable: ...
     '''A function that takes a returns.Result and returns a boolean. Required. Default: Return value of ``default_retryable``.'''
 
     # TODO: Make a protocol for this function
-    #next_wait_interval: Callable = default_next_wait_interval
-    next_wait_interval: Callable
+    @property
+    def next_wait_interval(self) -> Callable: ...
     '''A function that takes an integer number of seconds to wait and returns a new interval. Required. Default: Return value of ``default_next_wait_interval``.'''
 
-    domain: str
+    @property
+    def domain(self) -> str: ...
     '''Domain of a Pure API server. Required.'''
 
-    base_path: str
+    @property
+    def base_path(self) -> str: ...
     # TODO: Improve this documentation
     '''Base path of the Pure API URL entry point, without the version number segment. '''
 
     # TODO: We may not need this or want to require it for APIs other than Pure's
-    version: str
+    @property
+    def version(self) -> str: ...
     '''Pure API version, without the decimal point. For example, ``517`` for version 5.17.
     Default: Return value of ``default_version()``.'''
 
-    key: str
+    @property
+    def key(self) -> str: ...
     '''API key. Required.'''
 
-    headers: PMap
+    @property
+    def headers(self) -> PMap: ...
     '''HTTP headers. The constructor automatically adds an ``api-key`` header, using the value of
     the ``key`` attribute.'''
 
-    records_per_request: int
+    #records_per_request: int
 
-    offset_request_params_parser: OffsetRequestParamsParser
+    @property
+    def offset_request_params_parser(self) -> OffsetRequestParamsParser: ...
 
-    offset_response_parser: OffsetResponseParser
+    @property
+    def offset_response_parser(self) -> OffsetResponseParser: ...
 
-    base_url: str
+    @property
+    def base_url(self) -> str: ...
     '''Pure API entrypoint URL. Should not be included in constructor
     parameters. The constructor generates this automatically based on
     other attributes.'''
