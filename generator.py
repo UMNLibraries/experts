0f165b532defefafe2fb1b8c79deccfb28fe8cd7
David Naughton <naughton@pop-os.localdomain>
2023-11-20 21:13:33 -0600

    Adds experiments with send() to generator.py

--- generator.py
+++ generator.py
@@ -4,8 +4,21 @@ def to10():
         yield n
         n = n+1
         if n > 10:
-            #break
             return
 
 for n in to10():
     print(n)
+
+def two_way(token):
+    print('Starting server...')
+    while True:
+        token = yield f'{token=}'
+        print(f'Received request {token}')
+
+c = two_way(None)
+next(c)
+tokens = ['2023-11-20'] + list(range(1,11))
+for token in tokens:
+    print(f'Sending request: {token}')
+    response = c.send(token)
+    print(f'Response: {response}')
