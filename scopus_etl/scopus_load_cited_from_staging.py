f3de15161b11ddce059258be8b4464b07d03f38d
David Naughton <nihiliad@gmail.com>
2025-08-06 08:02:44 -0500

    Modifies load-from-staging test apps to use new scopus metadata classes

--- scopus_etl/scopus_load_cited_from_staging.py
+++ scopus_etl/scopus_load_cited_from_staging.py
@@ -4,12 +4,19 @@ from datetime import datetime
 import json
 
 from experts_dw import db, scopus_json
+from experts_dw.scopus_json_collection_meta import \
+    get_collection_meta_by_local_name
 
 with db.cx_oracle_connection() as db_session:
     cursor = db_session.cursor()
+
+    meta = get_collection_meta_by_local_name(
+        cursor=cursor,
+        local_name='citation',
+    )
+
     scopus_json.load_documents_from_staging(
         cursor,
-        collection_local_name='abstract',
-        relation='cited'
+        meta=meta,
     )
     db_session.commit()
