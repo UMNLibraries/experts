e789bc9542958ee8adb46d1360a3ceca8c9a6e86
David Naughton <nihiliad@gmail.com>
2025-08-06 07:54:14 -0500

    Modifies Abstract API test app to use improved request results

--- scopus_download_abstract_by_id.py
+++ scopus_download_abstract_by_id.py
@@ -12,16 +12,20 @@ from experts.api.scopus import \
     ResponseParser as r_parser, \
     AbstractResponseBodyParser as ar_parser
     
-scopus_id = sys.argv[1]
+request_scopus_id = sys.argv[1]
 
 with Client() as session:
     # Abstract Retrieval API
     # content='core' excludes dummy records (404s), but not when retrieving
     # abstracts by scopus ID, apparently.
     #params = m(content='core', view='FULL')
-    params = m(view='FULL')
+
+    #params = m(view='FULL')
     #params = m(content='core', view='REF')
-    result = session.get(f'abstract/scopus_id/{scopus_id}', params=params)
+    #result = session.get(f'abstract/scopus_id/{scopus_id}', params=params)
+
+    response_scopus_id, result = session.get_abstract_by_scopus_id(request_scopus_id)
+    assert response_scopus_id == request_scopus_id
 
     if not is_successful(result):
         raise result.failure()
