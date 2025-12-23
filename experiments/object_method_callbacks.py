2c493f4995946ec83ff14f86333d2c04ff794d9e
David Naughton <nihiliad@gmail.com>
2025-12-23 08:44:39 -0600

    More oo-with-callbacks experiments

--- experiments/object_method_callbacks.py
+++ experiments/object_method_callbacks.py
@@ -6,17 +6,45 @@ class CallbackTester:
     foo: str
     bar: str
 
-    def callback(self, object_name: str):
+    def default_callback(self, object_name: str):
+        print(f'Default callback! {object_name=}')
         print(self.foo, self.bar, object_name)
+
+# This doesn't work!
+# Generates 'missing required positional argument errors, due to missing class/object context.
+#    def method_with_callback(self, object_name, callback=callback):
+#        return callback(object_name)
+#
+#ct1.method_with_callback('ct1')
+#def callback_override(object_name: str):
+#    print(f'Callback override! {object_name=}')
+#ct2.method_with_callback('ct1', callback=callback_override)
+
+# This works...
+#    def callback_override(self, object_name: str):
+#        print(f'Callback override! {object_name=}')
+
+    def method_with_callback(self, object_name, callback=None):
+        if not callback:
+            callback = self.default_callback
+        return callback(object_name)
+
+# ... and so does this:
+def callback_override(object_name: str):
+    print(f'Callback override! {object_name=}')
 
 ct1 = CallbackTester(foo='manchu', bar='stool')
 ct2 = CallbackTester(foo='kung', bar='luchador')
 
+ct1.method_with_callback('ct1')
+#ct2.method_with_callback('ct2', callback=ct2.callback_override)
+ct2.method_with_callback('ct2', callback=callback_override)
+
 def callback_runner(callback, object_name):
     callback(object_name)
 
-callback_runner(ct1.callback, 'ct1')
-callback_runner(ct2.callback, 'ct2')
+callback_runner(ct1.default_callback, 'ct1')
+callback_runner(callback_override, 'ct2')
 
 some_obj = SomeClass()
 
